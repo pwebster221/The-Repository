9ed46d8392eed2f7827da6a86b92597e48d4a6bc
Paul Gustav Webster II <Pwebster@PathsofReverence.com>
2024-09-04 09:16:50 -0400

    commit

--- Feeders/AstroAPI Sub Program/astroapi/config.py
+++ Feeders/AstroAPI Sub Program/astroapi/config.py
@@ -1,3 +1,11 @@
-NEO4J_URI = "bolt://localhost:7687"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "password"
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SWISSEPH_PATH = os.getenv("SWISSEPH_PATH", "/path/to/AstroAPI/swisseph/ephe")
+NEO4J_URI = os.getenv("bolt://localhost:7687")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
